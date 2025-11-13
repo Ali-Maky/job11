11c7fe975d560e24d3a6c9667309775787e9ec90
Ali-Maky <60918965+Ali-Maky@users.noreply.github.com>
2025-11-13 13:56:51 +0300

    Update main.tsx

--- jobfair-landing-firebase/src/main.tsx
+++ jobfair-landing-firebase/src/main.tsx
@@ -1,4 +1,6 @@
+
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import './index.css'; // <-- ADD THIS LINE
 createRoot(document.getElementById('root')!).render(<App />);
